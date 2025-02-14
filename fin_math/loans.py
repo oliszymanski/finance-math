3b48fbb0bdea0f7a8e30a67ab18ac4eed27ce69a
Oliwier Szymański <oliwier.szymanski112@gmail.com>
2025-02-14 17:41:07 +0100

    add: loans functions (2/2)

--- fin_math/loans.py
+++ fin_math/loans.py
@@ -1,3 +1,14 @@
 '''
 Chapter 5: loans
+Loans are lent money with rates (so that the bank makes more money on
+ledning).
 '''
+
+
+def calc_loan_rate( PV: float, r: float, n: float ):
+    return PV * ( r/( 1 - ( 1 + r )**(-n) ) )
+
+
+
+def calc_balance( PMT: float, r: float, n: float, k: float ):
+    return PMT * ( 1 - ( 1 + r )**( -( n-k ) ) ) / r
